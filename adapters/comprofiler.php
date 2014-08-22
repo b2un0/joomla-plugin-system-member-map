28ff3e4f32b0972722f1f4fad430b10e4e4ff4d2
b2un0 <branko.wilhelm@gmail.com>
2014-08-22 19:39:55 +0200

    fix: fitBounds problems with cached coords

---
+++ adapters/comprofiler.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * @author     Branko Wilhelm <branko.wilhelm@gmail.com>
+ * @link       http://www.z-index.net
+ * @copyright  (c) 2014 Branko Wilhelm
+ * @license    GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+defined('_JEXEC') or die;
+
+final class MemberMapAdapterComProfiler implements MemberMapAdapterInterface
+{
+    protected $params;
+
+    public function __construct(JRegistry &$params)
+    {
+        $this->params = $params;
+    }
+
+    public function getUsers()
+    {
+        if (!JComponentHelper::isEnabled('com_comprofiler')) {
+            JFactory::getApplication()->enqueueMessage(JText::sprintf('PLG_CONTENT_MEMBERMAP_SOURCE_NOT_ENABLED', 'Community Builder'), 'error');
+            return false;
+        }
+
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true)
+            ->select('DISTINCT u.id')
+            ->from('#__kunena_users AS ku')
+            ->join('INNER', '#__users AS u ON(u.id = ku.userid)')
+            ->where('u.block = 0')
+            ->where('ku.location != ' . $db->quote(''));
+
+        if ($usergroups = $this->params->get('usergroup')) {
+            $query->join('INNER', '#__user_usergroup_map AS g ON(u.id = g.user_id)');
+            $query->where('g.group_id IN(' . implode(',', $usergroups) . ')');
+        }
+
+
+        switch ($this->params->get('order', 'name')) {
+            default:
+            case 'name';
+                $query->order('u.name');
+                break;
+
+            case 'username':
+                $query->order('u.username');
+
+                break;
+
+            case 'userid':
+                $query->order('u.id');
+                break;
+
+            case 'location':
+                $query->order('ku.location');
+                break;
+
+            case 'random':
+                $query->order('RAND()');
+                break;
+        }
+
+        $db->setQuery($query);
+
+        $members = $db->loadColumn();
+
+        if (empty($members)) {
+            return null;
+        }
+
+        $location_field = '??'; // TODO
+
+        $users = array();
+        foreach ($members as $key => $member) {
+            $user_id = $member;
+
+            $member = CBuser::getInstance($member);
+
+            $users[$key] = new stdClass;
+            $users[$key]->name = $member->getField('name', null, 'csv', 'none', 'list');
+            $users[$key]->address = $member->getField('avatar', null, 'csv', 'none', 'list');
+            $users[$key]->url = CBframework::framework()->userProfileEditUrl($user_id);
+            $users[$key]->requests = 0;
+            $users[$key]->ready = false;
+
+            if ($this->params->get('avatar', 1)) {
+                $users[$key]->avatar = $member->getField('avatar', null, 'csv', 'none', 'list');
+                if (!filter_var($users[$key]->avatar, FILTER_VALIDATE_URL)) {
+                    $users[$key]->avatar = JUri::root() . $users[$key]->avatar;
+                }
+            }
+        }
+
+        return $users;
+    }
+}
